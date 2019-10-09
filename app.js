044dc230e17ddb78c3b30eb085d36ad013234839
Jay VanOrd <jvanord@indasysllc.com>
2019-10-08 22:00:58 -0400

    a rough start

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-var app = {};
+app = {
+	onLoaded: function (){
+		document.getElementById('loading').style.display = 'none';
+		document.getElementById('main').classList.remove('hidden');
+	},
+	run: function(){
+		setTimeout(app.onLoaded, 3000);
+	}
+};
+app.run();
