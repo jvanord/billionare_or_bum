ade8b95b247304707974f57ae49d85a62c76fc7d
Jay VanOrd <jvanord@indasysllc.com>
2019-10-13 15:12:50 -0400

    some progress - all lumped together

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+DATA_KEY = 'dae90f9e-2a71-4e0e-bb3c-ed08e7c673cb';
 app = {
 	onLoaded: function (){
 		document.getElementById('loading').style.display = 'none';
 		document.getElementById('main').classList.remove('hidden');
 	},
 	run: function(){
+		if (!window.jQuery) throw new Error('JQuery didn\'t load - it\'s all fucked up.');
 		setTimeout(app.onLoaded, 3000);
 	}
 };
-app.run();
