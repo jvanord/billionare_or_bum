4f304f9f36892d226d6290170eba729ed3051110
Jay VanOrd <jvanord@indasysllc.com>
2019-10-16 21:08:51 -0400

    filled out October

--- update.js
+++ update.js
@@ -33,6 +33,118 @@ var UPDATE = {
 			image: 'https://st2.depositphotos.com/3001967/10941/v/600/depositphotos_109419702-stock-video-lonely-poor-man-lying-on.jpg',
 			correctAnswer: 'BUM',
 			points: 20
+		},
+		{
+			qid: 5,
+			available: '2019-10-17',
+			image: "i/3.jpg",
+			description: "This is nobody. Someone took a picture of her and put it on a stock photography website. It might be his girlfriend. Ai biet.",
+			correctAnswer: "BUM",
+			points: 20
+		},
+		{
+			qid: 6,
+			available: '2019-10-18',
+			image: "i/5.jpg",
+			description: "Mohammed Al Amoudi owns businesses in Saudi Arabia, Ethiopia, and Sweden. He's worth about $8.1 billion.",
+			correctAnswer: "BILLIONAIRE",
+			points: 20
+		},
+		{
+			qid: 7,
+			available: '2019-10-20',
+			image: "i/4.jpg",
+			description: "This is the famous Ukrainian 'stylish homeless man', Slavik. Google said he was famous anyway. I've never heard of him.",
+			correctAnswer: "BUM",
+			points: 20
+		},
+		{
+			qid: 8,
+			available: '2019-10-21',
+			image: "i/7.jpg",
+			description: "This is Mary-Kate Olsen. She and her twin, Ashley, were famous child actresses who now own a fashion emptire in New York. They hate publicity.",
+			correctAnswer: "BILLIONAIRE",
+			points: 20
+		},
+		{
+			qid: 9,
+			available: '2019-10-22',
+			image: "i/2.jpg",
+			description: "This is Tiger Woods, the famous golfer, when he got arrested. Even though he's been to jail, he's earned over $1.5 Billion in his lifetime ... playing golf. Golf!",
+			correctAnswer: "BILLIONAIRE",
+			points: 20
+		},
+		{
+			qid: 10,
+			available: '2019-10-23',
+			image: "i/6.jpg",
+			description: "This is some woman named Lisa Sawyer. She lives on the streets in Seattle. The local news took a picture of her for a story.",
+			correctAnswer: "BUM",
+			points: 20
+		},
+		{
+			qid: 11,
+			available: '2019-10-24',
+			image: "i/8.jpg",
+			description: "Another stock photo image. He looks stylish, sure. But he's nobody.",
+			correctAnswer: "BUM",
+			points: 20
+		},
+		{
+			qid: 12,
+			available: '2019-10-25',
+			image: "i/12.jpg",
+			description: "He looks like a tech billionaire, right? Nope. Just some guy.",
+			correctAnswer: "BUM",
+			points: 20
+		},
+		{
+			qid: 13,
+			available: '2019-10-26',
+			image: "i/1.jpg",
+			description: "Suhail Bahwan is from Oman and is worth $4.1 billion.",
+			correctAnswer: "BILLIONAIRE",
+			points: 20
+		},
+		{
+			qid: 14,
+			available: '2019-10-27',
+			image: "i/1.png",
+			description: "This is Jack Ma, the founder of Alibaba. He's worth $37.3 Billion. You should make an Alibaba like him.",
+			correctAnswer: "BILLIONAIRE",
+			points: 20
+		},
+		{
+			qid: 15,
+			available: '2019-10-28',
+			image: "i/9.jpg",
+			description: "This is Jay-Z. You might know him as Beyonc&eacute;'s husband, but he's actually famous for a lot of things.",
+			correctAnswer: "BILLIONAIRE",
+			points: 20
+		},
+		{
+			qid: 16,
+			available: '2019-10-29',
+			image: "i/10.jpg",
+			description: "Another nobody. I like his turban, though. I should wear a turban.",
+			correctAnswer: "BUM",
+			points: 20
+		},
+		{
+			qid: 17,
+			available: '2019-10-30',
+			image: "i/1.webp",
+			description: "This is Melinda Gates, wife of Bill Gates and found of the 'Bill and Melinda Gates Foundation.",
+			correctAnswer: "BILLIONAIRE",
+			points: 20
+		},
+		{
+			qid: 18,
+			available: '2019-10-31',
+			image: "i/11.jpg",
+			description: "Absolutely nobody. He does have a good hairstylist, though. I wish my hair looked like that.",
+			correctAnswer: "BUM",
+			points: 20
 		}
 	]
 };
